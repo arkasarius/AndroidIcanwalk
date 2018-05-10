58b7cb015c7e1a77b73abc920ee0f81710648618
arkasarius <roger.ordonez.brumos@gmail.com>
2018-05-10 09:38:53 +0200

    Implemented Gos Class

--- app/src/main/java/icanwalk/ordonez/roger/icanwalk/Gos.java
+++ app/src/main/java/icanwalk/ordonez/roger/icanwalk/Gos.java
@@ -1,4 +1,47 @@
 package icanwalk.ordonez.roger.icanwalk;
 
 public class Gos {
+
+
+    /*
+        Nom			[String]
+        Usuari amo 		[Usuari ID]
+        informació d'interés	[String]
+        */
+    private String nom;
+    private String amo;
+    private String bio;
+
+
+    public Gos(String nom, String amo, String bio) {
+        this.nom = nom;
+        this.amo = amo;
+        this.bio = bio;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAmo() {
+        return amo;
+    }
+
+    public void setAmo(String amo) {
+        this.amo = amo;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
 }
